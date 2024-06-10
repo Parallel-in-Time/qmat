d871523c6701622c04a6c2530d031f153c7a35db
Thibaut Lunet <thibaut.lunet@tuhh.de>
2024-06-10 18:34:45 +0200

    TL: back to simpler stuff

--- qmat/__init__.py
+++ qmat/__init__.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Generation of Q-coefficients for Spectral Deferred Corrections (and other time-integration methods ...)
-"""
-__version__ = "0.0.1"
-
 from qmat.qcoeff import genQCoeffs, Q_GENERATORS
 from qmat.qdelta import genQDeltaCoeffs, QDELTA_GENERATORS
 
