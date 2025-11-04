5441d0676ee9b73113ef73943463c467b10873fc
Thibaut Lunet <thibaut.lunet@tuhh.de>
2025-11-04 15:34:14 +0100

    TL: added imex stability scripts for thomas

--- qmat/playgrounds/tibo/__init__.py
+++ qmat/playgrounds/tibo/__init__.py
@@ -2,4 +2,6 @@
 - :class:`orthogonalPolynomials` : generate orthogonal polynomial values from any distribution.
 - :class:`lorenz` : application example of the generic solvers to solve the Lorenz equations.
 - :class:`imex` : starting development for the IMEX generic solvers.
+- :class:`imexStabilityRK` : investigating IMEX stability for RK methods
+- :class:`imexStabilitySDC` : investigating IMEX stability for SDC methods
 """
